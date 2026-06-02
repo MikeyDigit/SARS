4c19755dd80e0d415ef33161f7a6cc51afa79e7e
Michael <152760484+MikeyDigit@users.noreply.github.com>
2026-06-01 22:09:16 -0400

    Add full project main entry point

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+// SARS PROJECT: Evan Brown: 400627260 | Michael Di Giacinto: 400633391 | Ramsy Yu: 400639220
+#include "StudentRankingSystem.h"
+
+int main() {
+    StudentRankingSystem system;
+    system.runMenu();
+    return 0;
+}
